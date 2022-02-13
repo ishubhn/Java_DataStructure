ffc1fe2bfd66affdbebf8b0c6cf72f4c1ae17d14
Shubham Nagre <shubhamnagre14@gmail.com>
2022-02-13 11:35:50 +0530

    Changes in code - fixed trapped value

--- src/com/java/array/TrappedRainWater.java
+++ src/com/java/array/TrappedRainWater.java
@@ -3,6 +3,8 @@
  */
 package com.java.array;
 
+import java.util.Arrays;
+
 /**
  *
  */
@@ -33,21 +35,25 @@ public class TrappedRainWater {
 		//Consider all items running in o(N) running time
 		//min(maxLeft, maxRight) - height
 		int trapped = 0;
+
+//		System.out.println("Left max array:  " + Arrays.toString(maxLeft));
+//		System.out.println("Right Max array: " + Arrays.toString(maxRight));
+//		System.out.println("Buliding Block:  " + Arrays.toString(height));
 		
 		for (int i = 1; i < height.length - 1; i++) {
 			//only if max value from maxLeft, minLeft is greater than height
 			if (Math.min(maxLeft[i], maxRight[i]) > height[i]) {
 				trapped += Math.min(maxLeft[i], maxRight[i]) - height[i];
-				return trapped;
 			}
 		}
 		
-		return 0;
+		return trapped;
+		
 	}
 	
 	
 	public static void main(String[] args) {
-		int[] height = {2,1,3,1,4};
+		int[] height = {1,0,2,1,3,1,2,0,3};
 		
 		TrappedRainWater tr = new TrappedRainWater();
 		
